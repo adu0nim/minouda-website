453505515c73644aa45708d4606a2cb1d2f45da7
adu0nim <github@abeilleverte.ovh>
2026-04-04 20:55:32 +0200

    Quartz sync: Apr 4, 2026, 8:55 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,6 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
 HEAD
-origin/main
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
